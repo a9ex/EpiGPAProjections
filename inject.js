c4efd4fb54fbeb92da808f512bd1570b840cad0d
A1ex <a1ex@a1ex.fr>
2024-04-12 02:09:36 +0200

    feat: compute GPA credits from API

--- inject.js
+++ inject.js
@@ -2,9 +2,13 @@ console.log("Extension loaded!")
 
 console.log("Injecting script...")
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function getIntranetCookie() {
     const repsonse = await chrome.runtime.sendMessage({ command: "getIntranetCookie" });
-    return repsonse;
+    return repsonse.cookie ?? null;
 }
 
 function getIntranetLogin() {
@@ -21,7 +25,147 @@ function getIntranetLogin() {
         XPathResult.FIRST_ORDERED_NODE_TYPE,
         null
     ).singleNodeValue;
+
     return login ? login.textContent.trim() : null;
 }
 
-console.log(getIntranetLogin());
+async function injectTableColumn() {
+    await sleep(500);
+    const table = document.querySelector("#user-module > div:nth-child(1) > table");
+    const tableRows = table.querySelectorAll("tr");
+
+    if (tableRows[0].children[3].textContent.trim() === "Projection Grade") {
+        console.log("Intranet is so bad that it already has the column... skipping...")
+        return;
+    }
+
+    const newColumnHeader = document.createElement("th");
+    newColumnHeader.style.width = "75px";
+    newColumnHeader.innerHTML = '<div><div><span class="resizer"></span><span class="updown"></span><label>Projection Grade</label></div></div>';
+    tableRows[0].insertBefore(newColumnHeader, tableRows[0].children[3]);
+
+    const tableWhat = document.querySelector("#user-module > div.overflow > div > table");
+    const tableWhatRows = tableWhat.querySelectorAll("tr");
+
+    const newColumnHeaderWhat = document.createElement("th");
+    newColumnHeaderWhat.style.width = "75px";
+    tableWhatRows[0].insertBefore(newColumnHeaderWhat, tableWhatRows[0].children[3]);
+}
+
+async function injectTableData() {
+    await sleep(500);
+    const table = document.querySelector("#user-module > div.overflow > div > table");
+    const tableRows = table.querySelectorAll("tr");
+
+    for (let i = 1; i < tableRows.length; i++) {
+        const row = tableRows[i];
+        const studentId = row.children[0].textContent.trim();
+        const grade = row.children[3].textContent.trim();
+
+        const newColumn = document.createElement("td");
+        newColumn.className = "grade projection";
+        newColumn.setAttribute("tabindex", "0");
+        newColumn.innerHTML = `-`;
+        row.insertBefore(newColumn, row.children[3]);
+    }
+}
+
+async function injectGPA(gpa = "-") {
+    const noteZone = document.querySelector("#profil > div.bloc.top > div.rzone > span");
+
+    const gpaLabel = document.createElement("label");
+    gpaLabel.innerHTML = "G.P.A. Projection";
+    gpaLabel.style.color = "green";
+    noteZone.appendChild(gpaLabel);
+
+    const gpaValue = document.createElement("span");
+    gpaValue.className = "value";
+    gpaValue.style.color = "green";
+    gpaValue.innerHTML = gpa;
+    noteZone.appendChild(gpaValue);
+
+    const studentGrades = await getStudentGrades();
+    const computedGPA = computeGPA(studentGrades);
+    editGPA(computedGPA);
+}
+
+function editGPA(gpa) {
+    const gpaValue = document.querySelector("#profil > div.bloc.top > div.rzone > span > span:nth-child(6)");
+
+    if (!gpaValue) {
+        console.error("No custom GPA value found");
+        return;
+    }
+
+    gpaValue.innerHTML = gpa;
+}
+
+function computeGPA(grades) {
+    const gradeMappping = {
+        "A": 4,
+        "B": 3,
+        "C": 2,
+        "D": 1,
+    };
+
+    const gradesToBypass = ["-", "Acquis"];
+
+    const totalAcquiredCredits = grades.modules.reduce((acc, grade) => {
+        return acc + parseFloat(gradesToBypass.includes(grade.grade) ? 0 : grade.credits);
+    }, 0);
+
+    const gpa = grades.modules.reduce((acc, grade) => {
+        if (gradesToBypass.includes(grade.grade)) {
+            return acc;
+        }
+
+        const gradeValue = parseFloat(grade.credits);
+        const gradeWeight = parseFloat(gradeMappping[grade.grade] ?? 0);
+
+        return acc + (gradeValue * gradeWeight);
+    }, 0);
+
+    return (gpa / totalAcquiredCredits).toFixed(2);
+}
+
+async function getStudentGrades() {
+    const cookie = await getIntranetCookie();
+    const login = getIntranetLogin();
+
+    if (!cookie || !login) {
+        console.error("No cookie or login found");
+        return;
+    }
+
+    const response = await fetch(`https://intra.epitech.eu/user/${login}/notes?format=json`, {
+        headers: {
+            "Cookie": `user=${cookie},${document.cookie}`
+        }
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+window.navigation.addEventListener("navigate", (event) => {
+    const uri = event.destination.url;
+
+    if (uri.includes("/user/#!/notes")) {
+        console.log("Injecting table column and data...");
+        injectTableColumn();
+        injectTableData();
+    }
+});
+
+(() => {
+    if (window.location.href.includes("/user/#!/notes")) {
+        console.log("Injecting table column and data...");
+        injectTableColumn();
+        injectTableData();
+    }
+    if (window.location.href.includes("/user")) {
+        console.log("Injecting GPA...");
+        injectGPA();
+        getStudentGrades();
+    }
+})();
